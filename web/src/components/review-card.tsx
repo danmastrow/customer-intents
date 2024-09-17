5b577e073f30afe35670fd1fcc08a2424bd8cd16
danmastrow <15012375+danmastrow@users.noreply.github.com>
2024-09-17 18:31:12 +1000

    feat: update design of review card

--- web/src/components/review-card.tsx
+++ web/src/components/review-card.tsx
@@ -15,80 +15,82 @@ const ReviewCard = ({ intent, skip }: ReviewModalProps) => {
   const [isEditing, setIsEditing] = useState(false);
   return (
     <>
-      <Card className="mx-auto relative w-96 h-96">
-        <Button
-          className="absolute w-fit -right-16"
-          color="light"
-          pill
-          onClick={() => {
-            skip();
-            setIsEditing(false);
-          }}
-        >
-          Skip
-          <HiOutlineArrowRight className="ml-2 h-5 w-5" />
-        </Button>
-
-        <div className="flex justify-end px-4 pt-4">
+      <Card className="mx-auto relative sm:w-96 w-80">
+        <div>
           <Button
-            color="gray"
+            className="absolute w-fit bottom-1/2 -right-16"
+            color="light"
+            pill
             onClick={() => {
-              setIsEditing((prev) => !prev);
+              skip();
+              setIsEditing(false);
             }}
           >
-            {isEditing ? "Save" : "Edit"}
+            Skip
+            <HiOutlineArrowRight className="ml-2 h-5 w-5" />
+          </Button>
+
+          <div className="flex justify-end px-4 pt-4">
+            <Button
+              color="gray"
+              onClick={() => {
+                setIsEditing((prev) => !prev);
+              }}
+            >
+              {isEditing ? "Save" : "Edit"}
+            </Button>
+          </div>
+          <div className="flex flex-col items-center py-4">
+            <div className=" px-4 py-2 rounded flex flex-col justify-center items-center">
+              {isEditing ? (
+                <TextInput
+                  id="category"
+                  type="text"
+                  defaultValue={category}
+                  required
+                  className="py-2"
+                />
+              ) : (
+                <span className="text-base font-bold">{category}</span>
+              )}
+              {isEditing ? (
+                <Dropdown
+                  label={`${sentimentToEmoji(sentiment)} Sentiment`}
+                  dismissOnClick={false}
+                  defaultValue={sentiment}
+                  inline
+                  className="py-2"
+                >
+                  <Dropdown.Item value="Positive">
+                    {sentimentToEmoji("Positive")} Positive
+                  </Dropdown.Item>
+                  <Dropdown.Item value="Neutral">
+                    {sentimentToEmoji("Neutral")} Neutral
+                  </Dropdown.Item>
+                  <Dropdown.Item value="Negative">
+                    {sentimentToEmoji("Negative")} Negative
+                  </Dropdown.Item>
+                </Dropdown>
+              ) : (
+                <span className="text-xl">{sentimentToEmoji(sentiment)}</span>
+              )}
+            </div>
+
+            <h5 className="py-4 text-base text-center font-medium text-gray-900">
+              "{original_reason}"
+            </h5>
+          </div>
+          <Button
+            className="mt-auto mb-4 w-full"
+            color="blue"
+            onClick={() => {
+              skip();
+              setIsEditing(false);
+            }}
+          >
+            Confirm
           </Button>
         </div>
-        <div className="flex flex-col items-center py-4">
-          <div className=" px-4 py-2 rounded flex flex-col justify-center items-center">
-            {isEditing ? (
-              <TextInput
-                id="category"
-                type="text"
-                defaultValue={category}
-                required
-                className="py-2"
-              />
-            ) : (
-              <span className="text-base font-bold">{category}</span>
-            )}
-            {isEditing ? (
-              <Dropdown
-                label={`${sentimentToEmoji(sentiment)} Sentiment`}
-                dismissOnClick={false}
-                defaultValue={sentiment}
-                inline
-                className="py-2"
-              >
-                <Dropdown.Item value="Positive">
-                  {sentimentToEmoji("Positive")} Positive
-                </Dropdown.Item>
-                <Dropdown.Item value="Neutral">
-                  {sentimentToEmoji("Neutral")} Neutral
-                </Dropdown.Item>
-                <Dropdown.Item value="Negative">
-                  {sentimentToEmoji("Negative")} Negative
-                </Dropdown.Item>
-              </Dropdown>
-            ) : (
-              <span className="text-xl">{sentimentToEmoji(sentiment)}</span>
-            )}
-          </div>
-
-          <h5 className="py-4 text-base text-center font-medium text-gray-900">
-            "{original_reason}"
-          </h5>
-        </div>
-        <Button
-          className="mt-auto w-full"
-          color="blue"
-          onClick={() => {
-            skip();
-            setIsEditing(false);
-          }}
-        >
-          Confirm
-        </Button>
       </Card>
     </>
   );
--- web/src/pages/review-calls/page.tsx
+++ web/src/pages/review-calls/page.tsx
@@ -11,15 +11,22 @@ const ReviewCalls = () => {
   const [unreviewedIntents] = useAtom(unreviewedIntentsAtom);
 
   const selectedIntent = unreviewedIntents[selectedIndex];
+  const [remainingIntents] = useAtom(remainingUnreviewedIntentsAtom);
 
   return (
     <div className="p-4 md:ml-48">
       <div className="p-4 rounded-lg">
         <h1 className="text-3xl">Review calls</h1>
-        <h2 className="text-sm sm:text-md text-gray-500 mb-4">
+        <h2 className="text-sm sm:text-md text-gray-500">
           We've attempted to categorize and gauge the sentiment of callers,
           please help us improve our system by confirming them here.
         </h2>
+
+        <h3 className="my-4 flex w-full">
+          <span className="mx-auto ">
+            {remainingIntents} calls left to review.
+          </span>
+        </h3>
 
         <div className="flex w-full">
           {selectedIntent && (
